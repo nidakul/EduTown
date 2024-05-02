36a0c45fb1f765db85502631779f1d34c8cae848
Nida <nidakul@hotmail.com>
2024-05-02 20:24:16 +0300

    created city entity

---
+++ src/eduTown/Domain/Entities/City.cs
@@ -0,0 +1,15 @@
+﻿using NArchitecture.Core.Persistence.Repositories;
+using System;
+namespace Domain.Entities
+{
+    public class City : Entity<int>
+    {
+        public string Name { get; set; }
+
+        public City()
+        {
+        }
+    }
+}
+
+
