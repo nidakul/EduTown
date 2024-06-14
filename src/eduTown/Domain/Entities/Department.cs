980fd4cb2ab8aaefcfb8968e73d74b305b8fbbc6
nidakul <nidakul@hotmail.com>
2024-06-14 22:17:06 +0300

    created Department Entity

---
+++ src/eduTown/Domain/Entities/Department.cs
@@ -0,0 +1,16 @@
+﻿using NArchitecture.Core.Persistence.Repositories;
+using System;
+namespace Domain.Entities
+{
+    //Deparment for Instructor. Like Fen, Mat 
+    public class Department : Entity<int>
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+    }
+}
+
+
