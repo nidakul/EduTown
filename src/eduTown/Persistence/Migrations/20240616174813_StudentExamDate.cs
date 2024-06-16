68d31f781c4dcbd0c8dc9adf0d2047bd8b343ada
nidakul <nidakul@hotmail.com>
2024-06-16 20:48:49 +0300

    migration for StudentExamDate

---
+++ src/eduTown/Persistence/Migrations/20240616174813_StudentExamDate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class StudentExamDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudentExamDates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    ExamDateId = table.Column<int>(type: "int", nullable: false),
+                    StudentId1 = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentExamDates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StudentExamDates_ExamDates_ExamDateId",
+                        column: x => x.ExamDateId,
+                        principalTable: "ExamDates",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StudentExamDates_Students_StudentId1",
+                        column: x => x.StudentId1,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentExamDates_ExamDateId",
+                table: "StudentExamDates",
+                column: "ExamDateId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentExamDates_StudentId1",
+                table: "StudentExamDates",
+                column: "StudentId1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentExamDates");
+        }
+    }
+}
