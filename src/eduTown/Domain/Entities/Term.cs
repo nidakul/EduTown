5ff998e169480cca665cba5cd2feacb40b0135e3
nidakul <nidakul@hotmail.com>
2024-06-29 22:34:42 +0300

    created Term Entity

---
+++ src/eduTown/Domain/Entities/Term.cs
@@ -0,0 +1,15 @@
+﻿using NArchitecture.Core.Persistence.Repositories;
+using System;
+namespace Domain.Entities
+{
+    //Dönem bilgisi
+    public class Term : Entity<int>
+    {
+        public string Name { get; set; }
+
+        public Term()
+        {
+        }
+    }
+}
+
