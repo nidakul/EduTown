b37e69c673bf0c2297ecd39a4dbe1b69222de3d5
nidakul <nidakul@hotmail.com>
2024-07-17 22:55:39 +0300

    created SchoolClassBranch

---
+++ src/eduTown/Domain/Entities/SchoolClassBranch.cs
@@ -0,0 +1,19 @@
+﻿using NArchitecture.Core.Persistence.Repositories;
+using System;
+namespace Domain.Entities
+{
+    //sınıfların şubeleri
+    public class SchoolClassBranch : Entity<int>
+    {
+        public int SchoolClassId { get; set; }
+        public int BranchId { get; set; }
+
+        public virtual SchoolClass SchoolClass { get; set; }
+        public virtual Branch Branch { get; set; }
+
+        public SchoolClassBranch()
+        {
+        }
+    }
+}
+
